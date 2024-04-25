99b597924bbe5813505808001357a1e72c605fb6
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-04-25 20:43:41 +0200

    Added interface that works with css and js

---
+++ index.py
@@ -0,0 +1,11 @@
+import webview
+
+def create_app():
+    # Create a webview window
+    window = webview.create_window('My', 'index.html', width=800, height=600)
+
+    # Run the app
+    webview.start()
+
+if __name__ == '__main__':
+    create_app()
