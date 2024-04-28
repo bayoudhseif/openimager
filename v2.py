3e7be865026d38f42637f30302967c284c5c8410
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-04-28 20:30:54 +0200

    New interface

--- v2.py
+++ v2.py
@@ -2,7 +2,7 @@ import webview
 
 def create_app():
     # Create a webview window
-    window = webview.create_window('My', 'index.html', width=800, height=600)
+    window = webview.create_window('My', 'v2.html', width=800, height=600)
 
     # Run the app
     webview.start()
