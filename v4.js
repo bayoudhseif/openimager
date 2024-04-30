f8fedfc4fa5e0f3a8bedc1bbee34da47a40d65d0
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-04-30 09:36:27 +0200

    Fixed active color change & image switch

--- v4.js
+++ v4.js
@@ -1,18 +1,9 @@
 function addClass(component, className) {
-	var e = document.getElementById(component);
-	e.removeAttribute("class");
-	e.classList.add(className);
+    var e = document.getElementById(component);
+    e.removeAttribute("class");
+    e.classList.add(className);
 }
 
-function setAnim(e) {
-	var btn = document.getElementsByTagName("button");
-	for (i = 0; i < btn.length; i++) {
-		btn[i].classList.remove("active");
-	}
-	e.classList.toggle("active");
-	value = e.innerText;
-	addClass("component", value);
-}
 
 // first transition by default
 var btn = document.getElementsByTagName("button");
