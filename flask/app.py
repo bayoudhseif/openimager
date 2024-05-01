7d8ea978c38a766dccb9861718a4fc5532a86ef4
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-05-01 14:42:12 +0200

    changed code to reflect new folder structure in py

--- flask/app.py
+++ flask/app.py
@@ -2,24 +2,25 @@ from flask import Flask, render_template
 import subprocess
 
 app = Flask(__name__)
+app = Flask(__name__, template_folder='', static_folder='source')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route('/run_balance')
 def run_balance():
-    subprocess.Popen(["python", "balance.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    subprocess.Popen(["python", "levels/balance/balance.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
     return "balance.py is running in a separate window."
 
 @app.route('/run_agility')
 def run_agility():
-    subprocess.Popen(["python", "agility.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    subprocess.Popen(["python", "levels/agility/agility.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
     return "agility.py is running in a separate window."
 
 @app.route('/run_dexterity')
 def run_dexterity():
-    subprocess.Popen(["python", "dexterity.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    subprocess.Popen(["python", "levels/dexterity/dexterity.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
     return "dexterity.py is running in a separate window."
 
 if __name__ == '__main__':
