dc8c597bf90c67edf10f42d5e4e00e998ea7a74d
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-05-01 14:28:55 +0200

    Web version fully working with temporary placement

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/run_balance')
+def run_balance():
+    subprocess.Popen(["python", "balance.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    return "balance.py is running in a separate window."
+
+@app.route('/run_agility')
+def run_agility():
+    subprocess.Popen(["python", "agility.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    return "agility.py is running in a separate window."
+
+@app.route('/run_dexterity')
+def run_dexterity():
+    subprocess.Popen(["python", "dexterity.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    return "dexterity.py is running in a separate window."
 
 if __name__ == '__main__':
     app.run(debug=True)
