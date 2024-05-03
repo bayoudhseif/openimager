d03fd113576e63799bc196006eba827b94eef1d6
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-05-03 23:46:50 +0200

    fixed mac version

--- main-web/openimager-mac.py
+++ main-web/openimager-mac.py
@@ -1,27 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, rendertemplate
+import subprocess
 import os
 
-app = Flask(__name__)
-app = Flask(__name__, template_folder='', static_folder='')
+
+app = Flask(name, templatefolder='', staticfolder='')
 
 @app.route('/')
 def index():
-    return render_template('openimager.html')
+    return rendertemplate('openimager.html')
 
 @app.route('/run_balance')
 def run_balance():
-    os.system("open -a Terminal.app python levels/balance/balance.py")
+    python_path = "/Users/hichambzn/Desktop/t2/venv/bin/python"
+    script_path = "/Users/hichambzn/Desktop/t2/openimager/main-web/levels/balance/balance.py"
+    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
+    if result.returncode != 0:
+        return f"Failed to run balance.py: {result.stderr}"
     return "balance.py is running in a separate window."
 
 @app.route('/run_agility')
 def run_agility():
-    os.system("open -a Terminal.app python levels/agility/agility.py")
+    python_path = "/Users/hichambzn/Desktop/t2/venv/bin/python"
+    script_path = "/Users/hichambzn/Desktop/t2/openimager/main-web/levels/agility/agility.py"
+    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
+    if result.returncode != 0:
+        return f"Failed to run agility.py: {result.stderr}"
     return "agility.py is running in a separate window."
 
 @app.route('/run_dexterity')
 def run_dexterity():
-    os.system("open -a Terminal.app python levels/dexterity/dexterity.py")
+    python_path = "/Users/hichambzn/Desktop/t2/venv/bin/python"
+    script_path = "/Users/hichambzn/Desktop/t2/openimager/main-web/levels/dexterity/dexterity.py"
+    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
+    if result.returncode != 0:
+        return f"Failed to run dexterity.py: {result.stderr}"
     return "dexterity.py is running in a separate window."
 
-if __name__ == '__main__':
+if __name == '__main':
     app.run(debug=True)
