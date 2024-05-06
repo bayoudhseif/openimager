3272e23b6cc68fef70ee31e5f8aae7e2e14adcaf
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-05-06 02:22:50 +0200

    fixed flask bug

--- main-web/openimager_mac.py
+++ main-web/openimager_mac.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import subprocess
 import os
 
-app = Flask(__name__, templatefolder='', staticfolder='')
+app = Flask(__name__, template_folder='', static_folder='')
 
 @app.route('/')
 def index():
