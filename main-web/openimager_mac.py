d1a440f2b09d0787028198d7390a320770b495e3
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-05-06 02:07:15 +0200

    version for all, and for mac local directory

---
+++ main-web/openimager_mac.py
@@ -0,0 +1,40 @@
+from flask import Flask, rendertemplate
+import subprocess
+import os
+
+app = Flask(__name__, templatefolder='', staticfolder='')
+
+@app.route('/')
+def index():
+    return rendertemplate('openimager.html')
+
+@app.route('/run_balance')
+def run_balance():
+    python_path = "/Users/hichambzn/Desktop/prefinal/venv/bin/python"
+    script_path = "/Users/hichambzn/Desktop/prefinal/openimager/main-web/levels/balance/balance.py"
+    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
+    if result.returncode != 0:
+        error_message = result.stderr or "balance.py is running in a separate window."
+        return f"Failed to run balance.py: {error_message}"
+    return "balance.py executed successfully."
+
+@app.route('/run_agility')
+def run_agility():
+    python_path = "/Users/hichambzn/Desktop/prefinal/venv/bin/python"
+    script_path = "/Users/hichambzn/Desktop/prefinal/openimager/main-web/levels/agility/agility.py"
+    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
+    if result.returncode != 0:
+        return f"Failed to run agility.py: {result.stderr}"
+    return "agility.py is running in a separate window."
+
+@app.route('/run_dexterity')
+def run_dexterity():
+    python_path = "/Users/hichambzn/Desktop/prefinal/venv/bin/python"
+    script_path = "/Users/hichambzn/Desktop/prefinal/openimager/main-web/levels/dexterity/dexterity.py"
+    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
+    if result.returncode != 0:
+        return f"Failed to run dexterity.py: {result.stderr}"
+    return "dexterity.py is running in a separate window."
+
+if __name__ == '__main__':
+    app.run(debug=True)
