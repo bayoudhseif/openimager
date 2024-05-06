046ca660765cc652b71f67dfb06874969b124c99
Bayoudh Seif <bayoudh.pr@gmail.com>
2024-05-06 02:04:34 +0200

    Works for both mac and windows

--- main-web/openimager-mac.py
+++ main-web/openimager-mac.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import subprocess
 import os
+import sys
 
 app = Flask(__name__, template_folder='', static_folder='')
 
@@ -10,30 +11,33 @@ def index():
 
 @app.route('/run_balance')
 def run_balance():
-    python_path = "/Users/hichambzn/Desktop/t2/venv/bin/python"
-    script_path = "/Users/hichambzn/Desktop/t2/openimager/main-web/levels/balance/balance.py"
-    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
-    if result.returncode != 0:
-        return f"Failed to run balance.py: {result.stderr}"
+    python_path = sys.executable
+    script_path = os.path.join(os.getcwd(), "levels", "balance", "balance.py")
+    process = subprocess.Popen([python_path, script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        return f"Failed to run balance.py: {stderr.decode()}"
     return "balance.py is running in a separate window."
 
 @app.route('/run_agility')
 def run_agility():
-    python_path = "/Users/hichambzn/Desktop/t2/venv/bin/python"
-    script_path = "/Users/hichambzn/Desktop/t2/openimager/main-web/levels/agility/agility.py"
-    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
-    if result.returncode != 0:
-        return f"Failed to run agility.py: {result.stderr}"
+    python_path = sys.executable
+    script_path = os.path.join(os.getcwd(), "levels", "agility", "agility.py")
+    process = subprocess.Popen([python_path, script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        return f"Failed to run agility.py: {stderr.decode()}"
     return "agility.py is running in a separate window."
 
 @app.route('/run_dexterity')
 def run_dexterity():
-    python_path = "/Users/hichambzn/Desktop/t2/venv/bin/python"
-    script_path = "/Users/hichambzn/Desktop/t2/openimager/main-web/levels/dexterity/dexterity.py"
-    result = subprocess.run([python_path, script_path], capture_output=True, text=True)
-    if result.returncode != 0:
-        return f"Failed to run dexterity.py: {result.stderr}"
+    python_path = sys.executable
+    script_path = os.path.join(os.getcwd(), "levels", "dexterity", "dexterity.py")
+    process = subprocess.Popen([python_path, script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        return f"Failed to run dexterity.py: {stderr.decode()}"
     return "dexterity.py is running in a separate window."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
